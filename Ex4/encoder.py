343ee7816225cc6986fa63d589fac7382984e3de
Piotr Ładoński <piolado@gmail.com>
2020-09-20 15:02:36 +0200

    Add Ex4 with data and solution

---
+++ Ex4/encoder.py
@@ -0,0 +1,67 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from tensorflow.keras import layers, losses
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Model
+# %%
+# Load dataset
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+# %%
+class Autoencoder(Model):
+  def __init__(self, encoding_dim):
+    super(Autoencoder, self).__init__()
+    self.encoding_dim = encoding_dim   
+    self.encoder = tf.keras.Sequential([
+      layers.Flatten(),
+      layers.Dense(self.encoding_dim, activation='relu'),
+    ])
+    self.decoder = tf.keras.Sequential([
+      layers.Dense(28*28, activation='tanh'),
+      layers.Reshape((28, 28))
+    ])
+
+  def call(self, x):
+    encoded = self.encoder(x)
+    decoded = self.decoder(encoded)
+    return decoded
+
+latent_dim = 64 
+autoencoder = Autoencoder(latent_dim) 
+# %%
+autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
+# %%
+autoencoder.fit(x_train, x_train,
+                epochs=10,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+# %%
+encoded_imgs = autoencoder.encoder(x_test).numpy()
+decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+# %%
+n = 5
+plt.figure(figsize=(20, 4))
+for i in range(n):
+  # display original
+  ax = plt.subplot(2, n, i + 1)
+  plt.imshow(x_test[i])
+  plt.title("original")
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+
+  # display reconstruction
+  ax = plt.subplot(2, n, i + 1 + n)
+  plt.imshow(decoded_imgs[i])
+  plt.title("reconstructed")
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+plt.show()
+# %%
